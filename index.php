e7dc1858d4e6e072e94c696f1c15ea590f4d77b5
otukutun <orehaorz@gmail.com>
2014-07-21 16:59:10 +0900

    デザイン修正

--- index.php
+++ index.php
@@ -5,24 +5,51 @@
 
   <div class="starter-template">
     <?php include('template/session_message.php'); ?>
-    <h1>置き換えツール</h1>
     <div>
     <h2>文言登録</h2>
-    <form method="POST" action="save_rewrite_word.php">
-      元の単語:<input type='input' name='source_word' /><br>
-      置き換え単語:<input type='input' name='rewrite_words' /><br>
-      *複数置き換えたい単語がある場合は単語を「,」で区切ってください。<br>
-      *例)「東京,オリンピック」<br>
-      <input type='submit' value='登録'>
+    <form method="POST" class="form-horizontal" role="form" action="save_rewrite_word.php">
+      <div class="form-group">
+        <label for="inputSourceWord" class="col-sm-2 control-label">元の単語</label>
+        <div class="col-sm-10">
+          <input type='input' name='source_word' class="form-control" id="inputWourceWord" />
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="inputRewriteWords" class="col-sm-2 control-label">置き換え単語</label>
+        <div class="col-sm-10">
+          <input type="input" class="form-control" id="inputRewriteWords" name='rewrite_words'>
+          <br>*複数置き換えたい単語がある場合は単語を「,」で区切ってください。*例)「東京,オリンピック」<br>
+        </div>
+      </div>
+     <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="submit" class="btn btn-primary">登録</button>
+        </div>
+      </div>
     </form>
     </div>
     <div>
     <h2>置き換え</h2>
-  
-    <form method="POST" action="rewrite.php">
-      <textarea name='rewrite' cols='40' rowws='4'></textarea><br>
-      <input type='checkbox' name='shuffle' value='true'>文でシャッフル<br>
-      <input type='submit' value='変換'>
+    <form method="POST" class="form-horizontal" role="form" action="rewrite.php">
+      <div class="form-group">
+        <label for="inputRewrite" class="col-sm-2 control-label">元の単語</label>
+        <div class="col-sm-10">
+          <textarea name='rewrite' class="form-control" rows="3" id='inputRewrite'></textarea><br>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-offset-2 col-sm-10">
+            <div class="checkbox">
+              <label>
+                <input type='checkbox' name='shuffle' value='true'>文でシャッフル<br>
+              </label>
+            </div>
+          </div>
+        </div>
+       <div class="form-group">
+          <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-primary">変換</button>
+          </div>
+        </div>
     </form>
     </div>
     <?php include('template/replaced_sentence.php'); ?>  
--- template/header.php
+++ template/header.php
@@ -7,13 +7,11 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="#">リライトツール</a>
+    <a class="navbar-brand" href="/">リライトツール</a>
   </div>
   <div class="collapse navbar-collapse">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">ホーム</a></li>
-      <li><a href="#about">文言登録</a></li>
-      <li><a href="#contact">置き換え</a></li>
+      <li class="active"><a href="/">ホーム</a></li>
     </ul>
   </div><!--/.nav-collapse -->
 </div>
